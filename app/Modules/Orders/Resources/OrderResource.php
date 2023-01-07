055323d5edd8965c28e9fae6a0601c2aa600de71
TUR PC <alhrabi.tur1@gmail.com>
2023-01-08 02:46:57 +0300

    first commit

--- app/Modules/Orders/Resources/OrderResource.php
+++ app/Modules/Orders/Resources/OrderResource.php
@@ -31,9 +31,9 @@ class OrderResource extends JsonResource
             ],
             'products' => $this->whenLoaded('products', $this['products']),
             'coupon' => $this->whenLoaded('coupon', [
-                'code' => $this['coupon']['code'],
-                'discounted_amount' => $this['coupon']['discounted_amount'],
-                'amount' => $this['coupon']['type'] == 'Percentage' ?  $this['coupon']['amount'] . ' %' : ' SAR',
+                'code' => $this->coupon?->code,
+                'discounted_amount' => $this->coupon?->discounted_amount,
+                'amount' => $this->coupon?->type == 'Percentage' ?  $this->coupon?->amount . ' %' : ' SAR',
             ]),
             'address' => $this->whenLoaded('address', $this['address']),
         ];
--- app/Pages/ProductDetailPage/Resources/ProductDetailResource.php
+++ app/Pages/ProductDetailPage/Resources/ProductDetailResource.php
@@ -2,9 +2,10 @@
 
 namespace App\Pages\ProductDetailPage\Resources;
 
-use App\Modules\Users\Repository\UserRepository;
 use Illuminate\Support\Str;
+use App\Modules\Users\Repository\UserRepository;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Modules\Products\Services\ProductDiscountService;
 
 class ProductDetailResource extends JsonResource
 {
@@ -16,7 +17,16 @@ class ProductDetailResource extends JsonResource
      */
     public function toArray($request)
     {
+        $discountData = [
+            'price' =>  $this->price,
+            'discounted_price' => $this->discounted_price,
+            'discount_amount' =>  $this->discount_amount,
+            'discount_type' =>  $this->discount_type,
+            'discount_start_at' =>  $this->discount_start_at,
+            'discount_expires_at' =>   $this->discount_expires_at
+        ];
 
+        $discountService =  (new ProductDiscountService())->getDiscount($discountData);
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -24,10 +34,13 @@ class ProductDetailResource extends JsonResource
             'info_and_care' => $this->info_and_care,
             'details' => $this->details,
             'in_stock' => $this->stock > 0 ? true : false,
-            'price' =>  $this->price,
+
             'brand_name' => $this->brand_name,
             'brand_image' => $this->brand_image,
             'inWishlist' => in_array($this->id, app('inWishlist')),
+            'price' => $discountService['price'],
+            'price_before_discount' => $discountService['price_before_discount'],
+            'discount_amount' => $discountService['discount_amount'],
         ];
     }
 }
