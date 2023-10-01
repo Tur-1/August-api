f85fc2d6ca0cf070bd8ff1c0e53835ca4df47d6b
Turki Alharbi <63422322+Tur-1@users.noreply.github.com>
2023-10-01 17:36:43 +0300

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,6 +31,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
 
+               Route::group(base_path('routes/web.php'));
 
             Route::middleware('api')->prefix('api')
                 ->group(base_path('routes/AuthUserRoutes.php'));
